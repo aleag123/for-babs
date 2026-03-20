88933d97eb4f09e53e5d05315adc73a871f6d45d
Alejandro Arana-Garcia <alejandroaranagarcia19@gmail.com>
2026-03-20 19:24:50 -0400

    updated design + added photos

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
 const pageNumber = document.getElementById("pageNumber");
+const leftButton = document.querySelector(".nav-btn.left");
+const rightButton = document.querySelector(".nav-btn.right");
 
 function showPage(index) {
   pages.forEach((page) => page.classList.remove("active"));
   pages[index].classList.add("active");
   pageNumber.textContent = `${index + 1} / ${pages.length}`;
+
+  if (index == 0){
+    leftButton.style.display = "none";
+  } else {
+    leftButton.style.display = "block";
+  }
+
+  if (index == pages.length - 1) {
+    rightButton.style.display = "none";
+  } else {
+    rightButton.style.display = "block";
+  }
 }
 
 function nextPage() {
@@ -21,3 +35,5 @@ function prevPage() {
     showPage(currentPage);
   }
 }
+
+showPage(currentPage);
