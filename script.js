175a73af90662cfb146feeee77b49c96f44e60e3
Alejandro Arana-Garcia <alejandroaranagarcia19@gmail.com>
2026-03-20 17:39:26 -0400

    added photos folder

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
+const pageNumber = document.getElementById("pageNumber");
 
 function showPage(index) {
   pages.forEach((page) => page.classList.remove("active"));
   pages[index].classList.add("active");
+  pageNumber.textContent = `${index + 1} / ${pages.length}`;
 }
 
 function nextPage() {
